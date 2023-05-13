bbff6188b570e46609ee6aa74ce268e27cbe71c3
jkim000 <joshuakim514@gmail.com>
2023-05-12 18:10:22 -0700

    refactor: map out Cells in Gameboard

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,6 +1,21 @@
-import { ROW_SIZE, COLUMN_SIZE } from "../utils/constants";
+import { Cell } from "./Cell";
+
 const COLUMN_SIZE = 10;
 const ROW_SIZE = 20;
+
+const BOARD_HEIGHT = "1000px";
+const BOARD_WIDTH = "500px";
+
+const cellTypeMap: string[] = [
+    "background",
+    "o_block",
+    "i_block",
+    "z_block",
+    "s_block",
+    "l_block",
+    "j_block",
+    "t_block",
+];
 
 export const Gameboard = () => {
     const board: number[][] = new Array(ROW_SIZE); // board[rows][cols]
@@ -11,5 +26,22 @@ export const Gameboard = () => {
         board[i] = row;
     }
 
-    return <div>{board}</div>;
+    return (
+        <div className='flex justify-center w-full'>
+            <div
+                className={`flex flex-wrap w-[${BOARD_WIDTH}] h-[${BOARD_HEIGHT}] border border-1 border-black bg-slate-600`}
+            >
+                {board.map((row, rowIndex) => {
+                    return row.map((cell, cellIndex) => {
+                        return (
+                            <Cell
+                                key={`${rowIndex}-${cellIndex}`}
+                                type={cellTypeMap[cell]}
+                            />
+                        );
+                    });
+                })}
+            </div>
+        </div>
+    );
 };
