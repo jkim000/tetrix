29738c8a9c1729d93b6d8f346996a6967405f68a
jkim000 <joshuakim514@gmail.com>
2023-05-12 18:09:45 -0700

    feat: create initial version of a Cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import classNames from "classnames";
 
-interface CellType {
-    type: number;
+interface ColorMap {
+    [key: string]: string;
 }
 
+type CellType = typeof COLOR_MAP;
+
+const COLOR_MAP: ColorMap = {
+    background: "bg-zinc-900",
+    o_block: "bg-yellow-400",
+    i_block: "bg-blue-700",
+    z_block: "bg-green-600",
+    s_block: "bg-red-600",
+    l_block: "bg-orange-600",
+    j_block: "bg-pink-500",
+    t_block: "bg-purple-800",
+};
+
 export const Cell = ({ type }: CellType) => {
-    return <div>Cell</div>;
+    return (
+        <div
+            className={classNames(
+                COLOR_MAP[type],
+                "h-[48px] w-[48px] border border-1 justify-center"
+            )}
+        />
+    );
 };
