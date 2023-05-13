0ce23da65cdde93d1583fa004258ac0949ae92b5
jkim000 <joshuakim514@gmail.com>
2023-05-12 18:11:38 -0700

    style: add basic style to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Gameboard } from "./components/Gameboard";
 
 function App() {
     return (
-        <>
+        <div className='flex flex-col justify-center'>
             <h1>TETRIX</h1>
             <Gameboard />
-        </>
+        </div>
     );
 }
 
