e372e62ad3d58ed6b0179aedef7a11558eec1891
jkim000 <joshuakim514@gmail.com>
2023-06-05 21:28:19 -0700

    refactor: Cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,28 +1,21 @@
 import classNames from "classnames";
+import { TETROMINOS } from "../utils/tetromino";
 
-interface ColorMap {
-    [key: string]: string;
+interface CellProps {
+    type: string;
 }
 
-type CellType = typeof COLOR_MAP;
+export const Cell = ({ type }: CellProps) => {
+    const cellColor = TETROMINOS[type].color;
+    const borderTLColor = TETROMINOS[type].borderTLColor;
+    const borderBRColor = TETROMINOS[type].borderBRColor;
 
-const COLOR_MAP: ColorMap = {
-    background: "bg-zinc-900",
-    o_block: "bg-yellow-400",
-    i_block: "bg-blue-700",
-    z_block: "bg-green-600",
-    s_block: "bg-red-600",
-    l_block: "bg-orange-600",
-    j_block: "bg-pink-500",
-    t_block: "bg-purple-800",
-};
-
-export const Cell = ({ type }: CellType) => {
     return (
         <div
             className={classNames(
-                COLOR_MAP[type],
-                "h-[48px] w-[48px] border border-1 justify-center"
+                type !== "X" && `border-4 ${borderTLColor} ${borderBRColor}`,
+                cellColor,
+                "h-8 w-8 justify-center flex-none"
             )}
         />
     );
