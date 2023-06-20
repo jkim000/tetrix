291f84792281ed53fc8e540f808e01660912e71f
jkim000 <joshuakim514@gmail.com>
2023-06-19 21:54:16 -0700

    chore: typing stuff

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { createRandomTetromino } from "../utils/tetrisHelpers";
 import { TetrominoType } from "../utils/types";
 
-interface PlayerState {
+export interface PlayerState {
     pos: { x: number; y: number };
     tetromino: TetrominoType;
     collided: boolean;
 }
 
-export const usePlayer = () => {
+export const usePlayer = (): [PlayerState] => {
     const [player, setPlayer] = useState<PlayerState>({
         pos: { x: 0, y: 0 },
         tetromino: createRandomTetromino(),
--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { createBoard } from "../utils/tetrisHelpers";
+import { StageType } from "../utils/types";
+import { PlayerState } from "./usePlayer";
 
-export const useStage = () => {
-    const [stage, setStage] = useState(createBoard());
+export const useStage = (
+    player: PlayerState
+): [StageType, React.Dispatch<React.SetStateAction<StageType>>] => {
+    const [stage, setStage] = useState<StageType>(createBoard());
+
+    console.log("player:", player);
 
     return [stage, setStage];
 };
