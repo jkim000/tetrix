454ee5cf745cca3289e2f5274eb2eef372cf0569
jkim000 <joshuakim514@gmail.com>
2023-06-22 20:47:26 -0700

    refactor: useStage with more functionality

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createBoard } from "../utils/tetrisHelpers";
-import { StageType } from "../utils/types";
-import { PlayerState } from "./usePlayer";
+import { StageType, PlayerState } from "../utils/types";
 
 export const useStage = (
-    player: PlayerState
+    player: PlayerState,
+    resetPlayer?: () => void
 ): [StageType, React.Dispatch<React.SetStateAction<StageType>>] => {
     const [stage, setStage] = useState<StageType>(createBoard());
 
-    console.log("player:", player);
+    useEffect(() => {
+        const updateStage = (prevStage: StageType) => {
+            const newStage = prevStage.map((row) =>
+                row.map((cell) => (cell[1] === "clear" ? ["X", "clear"] : cell))
+            );
+
+            player.tetromino.forEach((row, y) => {
+                row.forEach((shape, x) => {
+                    if (shape !== "X") {
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            shape,
+                            `${player.hasCollided ? "merged" : "clear"}`,
+                        ];
+                    }
+                });
+            });
+
+            return newStage;
+        };
+
+        setStage((prev) => updateStage(prev));
+    }, [player]);
 
     return [stage, setStage];
 };
