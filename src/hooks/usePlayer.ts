f46c04e6e4442d6f55c13c29b0d781bf4020d0d3
jkim000 <joshuakim514@gmail.com>
2023-06-22 20:47:09 -0700

    refactor: usePlayer with more funcationality

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { createRandomTetromino } from "../utils/tetrisHelpers";
-import { TetrominoType } from "../utils/types";
+import { PlayerState } from "../utils/types";
 
-export interface PlayerState {
-    pos: { x: number; y: number };
-    tetromino: TetrominoType;
-    collided: boolean;
-}
-
-export const usePlayer = (): [PlayerState] => {
+export const usePlayer = () => {
     const [player, setPlayer] = useState<PlayerState>({
         pos: { x: 0, y: 0 },
-        tetromino: createRandomTetromino(),
-        collided: false,
+        hasCollided: false,
     });
 
-    return [player];
+    const updatePlayerPos = (x: number, y: number, hasCollided = false) => {
+        setPlayer((prev) => ({
+            ...prev,
+            pos: {
+                x: (prev.pos.x += x),
+                y: (prev.pos.y += y),
+            },
+            hasCollided,
+        }));
+    };
+
+    const resetPlayer = useCallback(() => {
+        const tetromino = createRandomTetromino().shape;
+        const centerValue = tetromino[0][1] === "L" ? 2 : 1;
+
+        setPlayer({
+            pos: { x: COL_SIZE / 2 - centerValue, y: 0 },
+            tetromino,
+            hasCollided: false,
+        });
+    }, []);
+
+    return { player, updatePlayerPos, resetPlayer };
 };
